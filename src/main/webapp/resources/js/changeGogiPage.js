48097897bcd59351cd69ce9a0d6a64d223410335
jhhong <jasper.jhhong@gmail.com>
2022-09-22 23:53:21 +0900

    Junghwa gogiwiki add

--- src/main/webapp/resources/js/changeGogiPage.js
+++ src/main/webapp/resources/js/changeGogiPage.js
@@ -5,10 +5,10 @@ $('.kind-button').click(function() {
     var activeTab = $(this).attr('name');
     $.ajax({
         type : 'GET',                 //get방식으로 통신
-        url : activeTab + ".html",    //탭의 data-tab속성의 값으로 된 html파일로 통신
+        url : "/html/" + activeTab + ".html",    //탭의 data-tab속성의 값으로 된 html파일로 통신
         dataType : "html",            //html형식으로 값 읽기
         error : function() {          //통신 실패시
-            alert('통신실패!');
+            alert('통신실패1!');
         },
         success : function(data) {    //통신 성공시 탭 내용담는 div를 읽어들인 값으로 채운다.
             $('#kind-image').html(data);
